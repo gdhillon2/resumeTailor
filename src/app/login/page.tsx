4e1442f1812cb442dba0bb50dacd1aecdbb35d67
gdhillon2 <gman.dhillon@gmail.com>
2024-10-12 10:34:50 -0700

    adjusted password auto form fillout color, also confirmed login page working, still need to set up user sessions and all thhat stuff after logging in

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,16 +1,51 @@
+"use client"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { Label } from "@/components/ui/label"
 import Link from "next/link"
+import { useState } from "react"
+import { supabase } from "@/lib/supabaseClient"
 
 export default function LoginPage() {
+    const [email, setEmail] = useState<string>("")
+    const [password, setPassword] = useState<string>("")
+    const [error, setError] = useState<string>("")
+
+    const handleLogIn = async () => {
+        const { error } = await supabase.auth.signInWithPassword({
+            email,
+            password
+        })
+
+        if (error) {
+            setError(error.message)
+        } else {
+            alert("Login successful!")
+        }
+    }
+
     return (
         <div className="flex flex-col gap-3 justify-center max-w-md w-full">
+            {error && <p>{error}</p>}
             <Label>Username</Label>
-            <Input />
+            <Input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
             <Label>Password</Label>
-            <Input type="password" />
-            <Button variant={"default"} size={"lg"}>Log In</Button>
+            <Input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button
+                variant={"default"}
+                size={"lg"}
+                onClick={handleLogIn}
+            >
+                Log In
+            </Button>
             <Label>Don't have an account?<Link href="/signup" className="text-blue-500"> Sign up now.</Link></Label>
         </div>
     )
